0fdc29740a49dddf342bed68dc019d76dc66e900
anna463 <57601829+anna463@users.noreply.github.com>
2019-11-25 20:21:39 -0600

    Add files via upload

---
+++ practiceVariables.py
@@ -0,0 +1,35 @@
+'''
+Created on Nov 10, 2019
+
+@author: ITAUser
+'''
+a = 4
+
+b = 3.45
+
+c = "string"
+
+a = True
+
+listInt = [1,2,2,3,4,5]
+
+print(listInt[3])
+
+listTwo = ["3", "4", "5"]
+
+listThree = [True, False, False]
+
+'''This is a boolean'''
+var = listThree[2]
+
+'''This is a list'''
+var = listThree
+
+string = "anna"
+
+name = string [0:3]
+print (name)
+name = string [:5]
+print (name)
+name = string [5:]
+print (name)
